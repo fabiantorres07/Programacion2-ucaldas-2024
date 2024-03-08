0ac603fa3769ecfc34722af5922fab4413e0b0c5
fabiantorres07 <fabian.torres47580@ucalds.edu.co>
2024-03-08 10:51:46 -0500

    Session4

--- Session4/Exercise1.java
+++ Session4/Exercise1.java
@@ -1,5 +1,15 @@
-public class Exercise1{
-    public static void main(String[] args) {
-        System.out.println("hola");
+public class Exercise1 {
+
+    int arraysize = 5;
+    int[] arrayvalues = new int[arraysize];
+    int[] arrayvalues2 = new int[5];
+    int[] arrayvalues3 = { 1, 2, 3, 4, 5 };
+
+    public int sumaArrayNumeros() {
+       int result = 0;
+       for (int i = 1; i <= arrayvalues3.length; i++){
+         result += arrayvalues3[i];
+       }
+       return result;
     }
 }
--- Session4/Main.java
+++ Session4/Main.java
@@ -1,5 +1,6 @@
 public class Main{
+    Exercise1 instans_exercise1 = new Exercise1();
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
     }
 }
