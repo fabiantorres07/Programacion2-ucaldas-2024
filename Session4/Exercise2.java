c122e53c17ff12e7db80db5ca7ad14982a6d13dc
fabiantorres07 <fabian.torres47580@ucalds.edu.co>
2024-03-08 11:24:46 -0500

    Session4

---
+++ Session4/Exercise2.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Exercise2 {
+    ArrayList<String> arrayListCadenas = new ArrayList<String>();
+}
--- Session4/Main.java
+++ Session4/Main.java
@@ -1,6 +1,6 @@
 public class Main{
-    Exercise1 instans_exercise1 = new Exercise1();
+     static Exercise1 instans_exercise1 = new Exercise1();
     public static void main(String[] args) {
-        
+        System.out.println(instans_exercise1.sumaArrayNumeros());
     }
 }
